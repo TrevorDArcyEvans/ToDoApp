fb9beee6bf7dc8034c8969b2685616eea7acd1c1
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-12-08 20:05:15 +0000

    - WebAPI

---
+++ ToDoAPI/Controllers/ToDoItemController.cs
@@ -0,0 +1,83 @@
+﻿namespace ToDoAPI.Controllers
+{
+	using Microsoft.AspNetCore.Mvc;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Models;
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public sealed class ToDoItemController : ControllerBase
+	{
+		private static readonly HashSet<ToDoItemModel> _toDoItems = new(new []
+		{
+			new ToDoItemModel { Id = 1, Description = "First description", IsCompleted = true },
+			new ToDoItemModel { Id = 2, Description = "Second description", IsCompleted = false },
+			new ToDoItemModel { Id = 3, Description = "Third description", IsCompleted = false },
+			new ToDoItemModel { Id = 4, Description = "Fourth description", IsCompleted = false },
+		});
+
+		// GET: api/ToDoItem
+		[HttpGet]
+		public ActionResult<IEnumerable<ToDoItemModel>> GetAll()
+		{
+			return Ok(_toDoItems.OrderBy(item => item.Id));
+		}
+
+		// GET: api/ToDoItem/5
+		[HttpGet("{id}")]
+		public ActionResult<ToDoItemModel> Get(int id)
+		{
+			var toDoItemModel = _toDoItems.FirstOrDefault(item => item.Id == id);
+			if (toDoItemModel is null)
+			{
+				return NotFound();
+			}
+
+			return toDoItemModel;
+		}
+
+		// POST: api/ToDoItem
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public ActionResult<ToDoItemModel> Create(ToDoItemModel toDoItemModel)
+		{
+			toDoItemModel.Id = _toDoItems.Any() ? _toDoItems.Max(item => item.Id) + 1 : 0;
+			_toDoItems.Add(toDoItemModel);
+
+			return Ok();
+		}
+
+		// PUT: api/ToDoItem
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut]
+		public ActionResult<ToDoItemModel> Update(ToDoItemModel toDoItemModel)
+		{
+			var updateModel = _toDoItems.FirstOrDefault(item => item.Id == toDoItemModel.Id);
+			if (updateModel is null)
+			{
+				return NotFound();
+			}
+
+			_toDoItems.Remove(updateModel);
+			_toDoItems.Add(toDoItemModel);
+
+			return Ok();
+		}
+
+		// DELETE: api/ToDoItem/5
+		[HttpDelete("{id}")]
+		public ActionResult Delete(int id)
+		{
+			var toDoItemModel = _toDoItems.FirstOrDefault(item => item.Id == id);
+			if (toDoItemModel is null)
+			{
+				return NotFound();
+			}
+
+			_ = _toDoItems.RemoveWhere(item => item.Id == id);
+
+			return Ok();
+		}
+	}
+}
---
+++ ToDoAPI/Models/ToDoItemModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models
+{
+	public sealed record ToDoItemModel
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Description { get; set; }
+
+		public bool IsCompleted { get; set; }
+
+		#region Equals overrides
+
+		public bool Equals(ToDoItemModel other)
+		{
+			if (ReferenceEquals(null, other))
+			{
+				return false;
+			}
+
+			if (ReferenceEquals(this, other))
+			{
+				return true;
+			}
+
+			return Id == other.Id;
+		}
+
+		public override int GetHashCode()
+		{
+			return Id;
+		}
+
+		#endregion
+	}
+}
